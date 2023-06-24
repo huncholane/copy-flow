a24b7b2eaf98cdd54c408684a80b3467e3e0e7c2
huncholane <huncho@ghostsystems.io>
2023-06-24 02:11:43 -0700

    works

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,68 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as clipboardy from "clipboardy";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const copyDisposable = vscode.commands.registerCommand(
+    "extension.copy",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("No active text editor found.");
+        return;
+      }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copy-flow" is now active!');
+      // Copy the editor's content to the clipboard
+      const content = editor.document.getText();
+      clipboardy.default.writeSync(content);
+      vscode.window.showInformationMessage(
+        "Editor content copied to clipboard."
+      );
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('copy-flow.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Copy Flow!');
-	});
+  const pasteDisposable = vscode.commands.registerCommand(
+    "extension.paste",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("No active text editor found.");
+        return;
+      }
 
-	context.subscriptions.push(disposable);
+      // Replace the editor's content with the clipboard content
+      const clipboardContent = clipboardy.default.readSync();
+      await editor.edit((editBuilder) => {
+        const documentRange = new vscode.Range(
+          editor.document.positionAt(0),
+          editor.document.positionAt(editor.document.getText().length)
+        );
+        editBuilder.replace(documentRange, clipboardContent);
+      });
+      vscode.window.showInformationMessage(
+        "Clipboard content pasted and replaced editor content."
+      );
+    }
+  );
+
+  context.subscriptions.push(copyDisposable);
+  context.subscriptions.push(pasteDisposable);
+
+  // Create and show the buttons in the toolbar
+  const copyButton = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left
+  );
+  copyButton.text = "$(clippy) Copy";
+  copyButton.command = "extension.copy";
+  copyButton.tooltip = "Copy editor content";
+  copyButton.show();
+
+  const pasteButton = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left
+  );
+  pasteButton.text = "$(diff-insert) Paste";
+  pasteButton.command = "extension.paste";
+  pasteButton.tooltip = "Paste clipboard content";
+  pasteButton.show();
 }
 
 // This method is called when your extension is deactivated
